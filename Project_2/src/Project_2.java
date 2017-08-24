9923e0d51b9c91fbbdfd9832ae29c52e92ee0b08
emilyhe0826 <18hej94@millshighsch.org>
2017-08-24 09:30:34 -0700

    APCS2

---
+++ Project_2/src/Project_2.java
@@ -0,0 +1,16 @@
+//Emily He
+//August 23,2017
+//Project_2 --- Use print and println
+/* this class compare output of print and println
+ */
+public class Project_2 {
+
+	public static void main(String[] args) {
+		System.out.print("Hi,there!");
+		System.out.println("Hello world");
+		System.out.println("Hello ");
+		
+
+	}
+
+}
