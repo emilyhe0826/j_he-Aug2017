cd25619696c5e9c8390b376c25f790c1acdb79d6
emilyhe0826 <18hej94@millshighsch.org>
2017-09-06 22:11:05 -0700

    new update

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -15,7 +15,7 @@ public class DoMath {
 		System.out.println(Calculate.discriminant(1,2,3));
 		System.out.println(Calculate.toimproperFrac(3,1,2));
 		System.out.println(Calculate.toMixedNum(7,2));
-		System.out.println(Calculate.foil(2,3,6,-7,n""));
+		System.out.println(Calculate.foil(2,3,6,-7,"n"));
 
 	}
 
