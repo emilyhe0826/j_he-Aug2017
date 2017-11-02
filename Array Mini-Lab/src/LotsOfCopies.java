3188798c5833210be85b6daa066790e957343f9b
emilyhe0826 <18hej94@millshighsch.org>
2017-11-01 20:35:15 -0700

    complete

--- Array Mini-Lab/src/LotsOfCopies.java
+++ Array Mini-Lab/src/LotsOfCopies.java
@@ -1,11 +1,42 @@
+import java.util.Arrays;
 /* Emily He
- * October 30, 2017
- * 
+ * October 31, 2017
+ * Array Mini_Lab Activity
+ * Lots of Copies 
  */
+
 public class LotsOfCopies {
-	public static void main(String[] args) {
+	public static void main (String [] args){
 		int num = 7;
-		String strMain="APCS";
-		int[] arrMain= {1,2,3,4,5};
+		//Test:
+		//int num2 = num;
+		//num = 5;
+		//System.out.println(num2);
+		
+		String strMain = "APCS";
+		
+		//State:
+		//String str2 = strMain;
+		//strMain = "hi";
+		//System.out.println(str2);
+		
+		int [] arrMain = {1,2,3,4,5};
+		
+		//int [] arr2 = arrMain;
+		//arrMain [0] = 19;
+		//System.out.println(Arrays.toString(arr2));
+		
+		changeMe(num, strMain, arrMain);
+		System.out.println(num);
+		System.out.println(strMain);
+		System.out.println(Arrays.toString(arrMain));
 	}
+	
+	public static void changeMe(int x, String str, int [] arr){
+		x = 20;
+		str = "Blue";
+		arr [0] = 19;
+	}
+	
+
 }
