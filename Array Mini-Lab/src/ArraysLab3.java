39b2a5c6230310a2deb4fef40f719d663c804ae9
emilyhe0826 <18hej94@millshighsch.org>
2017-11-05 21:12:02 -0800

    Nov.5

--- Array Mini-Lab/src/ArraysLab3.java
+++ Array Mini-Lab/src/ArraysLab3.java
@@ -1,25 +1,19 @@
 /*Emily He
- * November 1, 2017
+ * November 6, 2017
  * Create a class named ArraysLab3 that 
  * contains 6 static methods (main plus the 5 methods defined below).
 */
+
 import java.util.Arrays;
-/* Write a method sum that accepts two arrays of 
- *integers arr1 and arr2 and returns an array
- *of integers, in which every element is the sum 
- *of the elements at that index for the arrays arr1 and arr2. 
- *You can assume arrays arr1 and arr2 have at least one 
- *element each and are the same length.
-*/
 
 public class ArraysLab3 {
 	//Method 1
 	
-	/* Write a method sum that accepts two arrays of integers 
-	 *arr1 and arr2 and returns an array of integers, in which
-	 * every element is the sum of the elements at that index for 
-	 * the arrays arr1 and arr2. You can assume arrays arr1 and 
-	 * arr2 have at least one element each and are the same length.
+	/*
+	* Write a method named sum that accepts two arrays of integers arr1 and arr2 
+	* and returns an array of integers, 
+	* in which every element is the sum of the elements at that index for the arrays arr1 and arr2. 
+	* You can assume arrays arr1 and arr2 have at least one element each and are the same length.
 	*/
 	
 	public static int[] sum(int[] arr1, int[] arr2)
@@ -36,11 +30,13 @@ public class ArraysLab3 {
 	
 	//Method 2
 	
-	/*Write a method append that accepts an array of integers 
-	 * arr of length n and an integer num, and returns an array of 
-	 * integers of length n+1 that consists of the elements of arr with
-	 *num appended to the end. You can assume array arr has at least one element.
+	/*
+	* Write a method named append that accepts an array of integers arr of length n 
+	* and an integer num, and returns an array of integers of length n+1 
+	* that consists of the elements of arr with num appended to the end. 
+	* You can assume array arr has at least one element.
 	*/
+
 	
 	public static int[] append(int[] arr, int num)
 	{
@@ -55,11 +51,11 @@ public class ArraysLab3 {
 	
 	//Method 3
 	
-	/*Write a method append that accepts an array of integers 
-	 * arr of length n and an integer num, and returns an array of
-	 * integers of length n+1 that consists of the elements of arr 
-	 * with num appended to the end. You can assume array arr
-	 * has at least one element.
+	/*
+	* Write a method named remove that accepts an array of integers arr and an integer idx 
+	* and returns an array of integers consisting of all of the elements of arr 
+	* except for the element at index idx (thus, the returned array has a length of arr.length – 1). 
+	* You can assume arr has at least two elements.
 	*/
 	public static int[] remove(int[] arr, int idx){
 		assert (arr.length >= 2);
@@ -77,11 +73,10 @@ public class ArraysLab3 {
 	
 	//Method 4
 	
-	/*Write a method sumEven that accepts an array of integers 
-	* arr and returns an integer containing 
-	*the sum of the elements at the even indices of arr.  
-	*(That means elements at indices 0,2,4,6,8.) 
-	*You can assume arr has at least one element.
+	/*
+	* Write a method sumEven that accepts an array of integers arr 
+	* and returns an integer containing the sum of the elements at the even indices of arr. 
+	* (That means elements at indices 0,2,4,6,8.) You can assume arr has at least one element.
 	*/
 	
 	public static int sumEven(int[] arr){
@@ -97,12 +92,12 @@ public class ArraysLab3 {
 	
 	//Method 5
 	
-	/*Write a method rotateRight that accepts an array of 
-	 * integers arr and does not return a value. 
-	 * The rotateRight method moves each element of arr one
-	 *  index to the right (element 0 goes to element 1, 
-	 *  element 1 goes to element 2, …, element n-1 goes 
-	 *  to element 0). You can assume arr has at least one element.
+	/*
+	* Write a method rotateRight that accepts an array of integers arr 
+	* and does not return a value. 
+	* The rotateRight method moves each element of arr one index to the right 
+	* (element 0 goes to element 1, element 1 goes to element 2, …, element n-1 goes to element 0). 
+	* You can assume arr has at least one element.
 	*/
 	
 	public static void rotateRight(int[] arr){
@@ -138,9 +133,8 @@ public class ArraysLab3 {
 	a1
 	*/
 
-	
-	public static void main(String[] args) 
-	{
+
+	public static void main(String[] args) {
 		int[] a1 = {5, 10, 15, 20, 25, 30, 35, 40};
 		int[] a2 = {7, 14, 21, 28, 35, 42, 49, 56};
 		int[] sumArr = ArraysLab3.sum(a1, a2);
--- Array Mini-Lab/src/LotsOfCopies.java
+++ Array Mini-Lab/src/LotsOfCopies.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+
 /* Emily He
  * October 31, 2017
  * Array Mini_Lab Activity
@@ -8,34 +9,59 @@ import java.util.Arrays;
 public class LotsOfCopies {
 	public static void main (String [] args){
 		int num = 7;
-		//Test:
-		//int num2 = num;
-		//num = 5;
-		//System.out.println(num2);
-		
 		String strMain = "APCS";
-		
-		//State:
-		//String str2 = strMain;
-		//strMain = "hi";
-		//System.out.println(str2);
-		
 		int [] arrMain = {1,2,3,4,5};
 		
-		//int [] arr2 = arrMain;
-		//arrMain [0] = 19;
-		//System.out.println(Arrays.toString(arr2));
-		
 		changeMe(num, strMain, arrMain);
-		System.out.println(num);
-		System.out.println(strMain);
-		System.out.println(Arrays.toString(arrMain));
+		/*
+		System.out.println("The result in Main, num: "+ num);
+		System.out.println("The result in Main, strMain: "+strMain);
+		System.out.println("The result in Main, arrMain:"+ Arrays.toString(arrMain));
+		*/
+		
 	}
 	
 	public static void changeMe(int x, String str, int [] arr){
-		x = 20;
+		
+		/*PART 1
+		System.out.println("In changeMe, before the change, num (aka x): " + x);
+		x = 4;
+		System.out.println("In changeMe, after the change, num (aka x): " + x);
+		
+		System.out.println("In changeMe, before the change, strMain (aka str): " + str);
 		str = "Blue";
+		System.out.println("In changeMe, after the change, strMain (aka str): " + str);
+		
+		System.out.println("In changeMe, before the change, arrMain (aka arr): " + Arrays.toString(arr));
 		arr [0] = 19;
+		System.out.println("In changeMe, after the change, arrMain (aka arr): " + Arrays.toString(arr));
+		*/
+		
+		//PART 2
+		
+			//a=start
+			//b=a
+			//a==new
+			//what does b==?
+		
+			//integer
+			x=3;
+			int bint = x;
+			x=23;
+			System.out.println("b:"+ bint);
+			
+			//String
+			str="hhhh";
+			String bstr= str;
+			str="Ahhhhh";
+			System.out.println("bstr:"+ bstr);
+			//Integer and strings behave the same
+			
+			//Array
+			int [] barr= arr;
+			arr[0]=200;
+			System.out.println("barr:"+ Arrays.toString(barr));
+			//Unlike Integer and strings, Array just creates a new name.(Passing by reference)
 	}
 	
 
